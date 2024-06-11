0475e2e73fac917541d9ea1f0f9507c1d67edd7a
Jeff Brand <jeff.brand@outlook.com>
2024-06-11 14:09:44 -0500

    Add project files.

---
+++ TestWpf/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.PreviewKeyDown += Window_PreviewKeyDown;
+            this.PreviewKeyUp += Window_PreviewKeyUp;
+            this.SizeChanged += MainWindow_SizeChanged;
+           
+            this.Loaded += (s,e) => { };
+
+        }
+
+        private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+        
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+           
+        }
+
+        private void Window_PreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.LWin || e.Key == Key.RWin)
+            {
+           
+                this.ResizeMode = ResizeMode.NoResize;
+                this.WindowState = WindowState.Maximized;
+                e.Handled = true;
+            }
+        }
+    }
+}
